34fb1bcf8c6d3e374d31fac064cb85b7a1a64294
kamau simon <kamausimon217@gmail.com>
2024-08-22 15:00:10 +0300

    chore: Add user logout functionality and logging

--- Tasker-App/app/Http/Controllers/LogoutController.php
+++ Tasker-App/app/Http/Controllers/LogoutController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class LogoutController extends Controller
 {
@@ -14,6 +15,8 @@ class LogoutController extends Controller
 
         $request->session()->invalidate();
         $request->session()->regenerateToken();
+
+        Log::info('User logged out', ['user_id' => $request->user()->id]);
 
         return redirect()->route('/')->with('status', 'success logging user out');
     }
