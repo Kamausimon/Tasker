34dea7fc5d2d13d7daaf069a9b8fd29d94f3fcd9
kamau simon <kamausimon217@gmail.com>
2024-08-25 22:16:39 +0300

    refactor: Update login form action URL to use the new authentication route

--- Tasker-App/resources/views/auth/login.blade.php
+++ Tasker-App/resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('auth.loginUser') }}">
+    <form method="POST" action="/loginUser">
         @csrf
 
         <!-- Email Address -->
