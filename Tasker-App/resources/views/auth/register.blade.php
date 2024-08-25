dc1760a321f41721c6402232df419d4e207f55ce
kamau simon <kamausimon217@gmail.com>
2024-08-25 21:34:48 +0300

    refactor: Remove unused name field from registration form

--- Tasker-App/resources/views/auth/register.blade.php
+++ Tasker-App/resources/views/auth/register.blade.php
@@ -2,12 +2,7 @@
     <form method="POST" action="{{ route('auth.registerUser') }}">
         @csrf
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
+
 
         <!-- Email Address -->
         <div class="mt-4">
