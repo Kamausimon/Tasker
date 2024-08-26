1a06f82a79844cab465a09de89b6c2718dcf13c4
kamau simon <kamausimon217@gmail.com>
2024-08-26 14:42:25 +0300

    refactor: Update task index page layout and remove unnecessary code

--- Tasker-App/resources/views/task/index.blade.php
+++ Tasker-App/resources/views/task/index.blade.php
@@ -1,17 +1,16 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+ <!DOCTYPE html>
+ <html>
+ @include('partials._head')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-    </div>
-</x-app-layout>
+ <body>
+     @include('partials._nav')
+
+     <!-- main content -->
+     <div>
+         <h1>Task Dashboard</h1>
+     </div>
+
+     <!-- end of main content -->
+ </body>
+
+ </html>
