ce7f7380101d050ad64ea38f328f04633b23681b
kamau simon <kamausimon217@gmail.com>
2024-08-26 19:06:02 +0300

    refactor: Add Alpine.js CDN script for improved interactivity

--- Tasker-App/resources/views/Partials/_head.blade.php
+++ Tasker-App/resources/views/Partials/_head.blade.php
@@ -4,4 +4,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
 </head>
