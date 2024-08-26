6461903bff03d757e3d8b4e55c07789af27d6436
kamau simon <kamausimon217@gmail.com>
2024-08-26 14:42:03 +0300

    refactor: Update authentication and task route names and action URLs

--- Tasker-App/resources/views/layouts/app.blade.php
+++ Tasker-App/resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
 
 <body class="font-sans antialiased">
     <div class="min-h-screen bg-gray-100">
-        @include('layouts.navigation')
+
 
         <!-- Page Heading -->
         @isset($header)
