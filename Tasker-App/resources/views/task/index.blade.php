af14664cc8b4358fd9957e80e25aa5fc6b7bf4d7
kamau simon <kamausimon217@gmail.com>
2024-08-28 15:02:11 +0300

    refactor: Update task index page layout and sidebar navigation

--- Tasker-App/resources/views/task/index.blade.php
+++ Tasker-App/resources/views/task/index.blade.php
@@ -5,11 +5,23 @@
  <body class="bg-gray-700">
 
      @include('partials._nav')
-     <div class="grid grid-cols-5 ">
-         @include('partials._sidebar')
+     <div class="grid grid-cols-6 gap-0">
+         <div clas="col-span-1">
+             @include('partials._sidebar')
+         </div>
+
          <!-- main content -->
-         <div class="col-span-4">
-             <h1>Task Dashboard</h1>
+         <div class="col-span-5">
+             <div>
+                 <!-- min nav -->
+                 <div>
+                     @include('partials._minNav')
+                 </div>
+                 <!-- end of min nav -->
+                 <div>
+
+                 </div>
+             </div>
          </div>
 
          <!-- end of main content -->
