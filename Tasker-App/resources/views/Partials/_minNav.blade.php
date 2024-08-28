3a985c4a33f3f0484994dd2b5c847348dfb6da23
kamau simon <kamausimon217@gmail.com>
2024-08-28 15:21:13 +0300

    refactor: Update minimal navigation bar and sidebar links for consistency

--- Tasker-App/resources/views/Partials/_minNav.blade.php
+++ Tasker-App/resources/views/Partials/_minNav.blade.php
@@ -1,8 +1,8 @@
 <div class="text-white bg-slate-800 ">
     <nav class="flex flex-row">
         <a href="{{route('dashboard')}}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700">Dashboard</a>
-        <a href="{{route('task.create')}}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700">Tasks</a>
-        <a href="{{route('project.create')}}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700">Projects</a>
+        <a href="{{route('task.create')}}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700">create Tasks</a>
+        <a href="{{route('project.create')}}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700">create Projects</a>
 
     </nav>
 </div>
