9682d6ae22e921ad9e8fcb21e4c7632e4f397316
kamau simon <kamausimon217@gmail.com>
2024-08-28 18:23:38 +0300

    refactor: Update task create page layout and content

--- Tasker-App/resources/views/task/create.blade.php
+++ Tasker-App/resources/views/task/create.blade.php
@@ -1,5 +1,57 @@
 @extends('task.index')
 
 @section('content')
-<h1>create tasks</h1>
+<div>
+    <form action="">
+        <!-- title -->
+        <div>
+            <label for=""></label>
+            <input type="text">
+            @error('title')
+            <span class="text-red-500 text-xs italic">{{ $message }}</span>
+            @enderror
+        </div>
+
+        <!-- description -->
+        <div>
+            <label for=""></label>
+            <input type="text">
+            @error('title')
+            <span class="text-red-500 text-xs italic">{{ $message }}</span>
+            @enderror
+        </div>
+
+        <!-- completed -->
+        <div>
+            <label for=""></label>
+            <input type="checkbox">
+            @error('title')
+            <span class="text-red-500 text-xs italic">{{ $message }}</span>
+            @enderror
+        </div>
+
+        <!-- due at -->
+        <div>
+            <label for=""></label>
+            <input type="date">
+            @error('title')
+            <span class="text-red-500 text-xs italic">{{ $message }}</span>
+            @enderror
+        </div>
+
+        <!-- priority -->
+        <div>
+            <label for="priority">Priority</label>
+            <select id="priority" name="priority">
+                <option value="low">Low</option>
+                <option value="medium">Medium</option>
+                <option value="high">High</option>
+            </select>
+            @error('priority')
+            <span class="text-red-500 text-xs italic">{{ $message }}</span>
+            @enderror
+        </div>
+
+    </form>
+</div>
 @endsection
