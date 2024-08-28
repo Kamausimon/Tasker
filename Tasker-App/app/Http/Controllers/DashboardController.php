b59bf0ffb639bcee111fad7eac6cc3f23220764a
kamau simon <kamausimon217@gmail.com>
2024-08-28 15:01:59 +0300

    refactor: Add tasks fetching to DashboardController index action

--- Tasker-App/app/Http/Controllers/DashboardController.php
+++ Tasker-App/app/Http/Controllers/DashboardController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Task;
+use Illuminate\Support\Facades\Log;
 
 class DashboardController extends Controller
 {
     //
+    public function index()
+    {
+        $tasks = Task::all();
+        Log::info('tasks fetched');
+
+        return view('dashboard', [$tasks => 'tasks']);
+    }
 }
