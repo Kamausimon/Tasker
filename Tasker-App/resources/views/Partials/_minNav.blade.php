ba647fe9d06643a9c73bb30a0b1886460e593b87
kamau simon <kamausimon217@gmail.com>
2024-08-29 16:35:55 +0300

    refactor: Update minimal navigation bar to include user ID in dashboard link

--- Tasker-App/resources/views/Partials/_minNav.blade.php
+++ Tasker-App/resources/views/Partials/_minNav.blade.php
@@ -1,6 +1,6 @@
 <div class="text-white bg-slate-800">
     <nav class="flex flex-row">
-        <a href="{{ route('dashboard') }}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700 {{ Request::routeIs('dashboard') ? 'bg-gray-700' : '' }}">Dashboard</a>
+        <a href="{{ route('dashboard',['id' => Auth::id()] ) }}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700 {{ Request::routeIs('dashboard') ? 'bg-gray-700' : '' }}">Dashboard</a>
         <a href="{{ route('task.create') }}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700 {{ Request::routeIs('task.create') ? 'bg-gray-700' : '' }}">Create Tasks</a>
         <a href="{{ route('project.create') }}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700 {{ Request::routeIs('project.create') ? 'bg-gray-700' : '' }}">Create Projects</a>
     </nav>
