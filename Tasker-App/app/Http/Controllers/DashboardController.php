1f8417bd16443f270e426099df39fb697f76578b
kamau simon <kamausimon217@gmail.com>
2024-08-29 16:35:17 +0300

    refactor: Update DashboardController index action to include additional task and project data

--- Tasker-App/app/Http/Controllers/DashboardController.php
+++ Tasker-App/app/Http/Controllers/DashboardController.php
@@ -5,15 +5,24 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Task;
 use Illuminate\Support\Facades\Log;
+use App\Models\Project;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     //
-    public function index()
+    public function index(string $id)
     {
-        $tasks = Task::all();
-        Log::info('tasks fetched');
+        $user = Auth::user();
+        $tasksCreated = Task::where('user_id', Auth::id())->get();
+        $tasksDueToday = Task::where('due_at', today())->where('user_id', Auth::id())->get();
+        $overdueItems = Task::where('due_at', '<', today())->where('completed', false)->where('user_id', Auth::id())->get();
+        $pendingTasks = Task::where('completed', false)->where('user_id', Auth::id())->get();
+        $projects = Project::where('user_id', Auth::id())->get();
 
-        return view('dashboard', ['tasks' => $tasks]);
+        $tasksInProgressCount = Task::where('user_id', $id)->where('completed', false)->count();
+        $finishedTasksCount = Task::where('user_id', $id)->where('completed', true)->count();
+
+        return view('dashboard', compact('user', 'tasksCreated', 'tasksDueToday', 'overdueItems', 'pendingTasks', 'projects', 'tasksInProgressCount', 'finishedTasksCount'));
     }
 }
