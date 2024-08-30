45f5b383c56d5d4deb2a704d7490338d2a360fb7
kamau simon <kamausimon217@gmail.com>
2024-08-30 20:15:15 +0300

    refactor: Update navigation links in _minNav.blade.php

--- Tasker-App/resources/views/Partials/_minNav.blade.php
+++ Tasker-App/resources/views/Partials/_minNav.blade.php
@@ -1,7 +1,7 @@
 <div class="text-white bg-slate-800">
     <nav class="flex flex-row">
         <a href="{{ route('dashboard',['id' => Auth::id()] ) }}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700 {{ Request::routeIs('dashboard') ? 'bg-gray-700' : '' }}">Dashboard</a>
-        <a href="{{ route('task.create') }}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700 {{ Request::routeIs('task.create') ? 'bg-gray-700' : '' }}">Create Tasks</a>
-        <a href="{{ route('project.create') }}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700 {{ Request::routeIs('project.create') ? 'bg-gray-700' : '' }}">Create Projects</a>
+        <a href="{{ route('task.create') }}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700 {{ Request::routeIs('task.create') ? 'bg-gray-700' : '' }}"> Tasks</a>
+        <a href="{{ route('project.create') }}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700 {{ Request::routeIs('project.create') ? 'bg-gray-700' : '' }}"> Projects</a>
     </nav>
 </div>
