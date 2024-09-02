b0a1e08ec491ddafaff8b187dba83c0d52bc5c98
kamau simon <kamausimon217@gmail.com>
2024-09-02 06:15:12 +0300

    Update delete button in all.blade.php view for tasks

--- Tasker-App/resources/views/task/all.blade.php
+++ Tasker-App/resources/views/task/all.blade.php
@@ -17,9 +17,10 @@
                 <form action="{{ route('task.destroy', $task->id) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this task?');">
                     @csrf
                     @method('DELETE')
-                    <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24">
-                        <path fill="grey" d="M7.616 20q-.672 0-1.144-.472T6 18.385V6H5V5h4v-.77h6V5h4v1h-1v12.385q0 .69-.462 1.153T16.384 20zM17 6H7v12.385q0 .269.173.442t.443.173h8.769q.23 0 .423-.192t.192-.424zM9.808 17h1V8h-1zm3.384 0h1V8h-1zM7 6v13z" />
-                    </svg>
+                    <button type="submit" class="text-red-400 hover:underline"> <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24">
+                            <path fill="grey" d="M7.616 20q-.672 0-1.144-.472T6 18.385V6H5V5h4v-.77h6V5h4v1h-1v12.385q0 .69-.462 1.153T16.384 20zM17 6H7v12.385q0 .269.173.442t.443.173h8.769q.23 0 .423-.192t.192-.424zM9.808 17h1V8h-1zm3.384 0h1V8h-1zM7 6v13z" />
+                        </svg></button>
+
                 </form>
             </div>
         </li>
