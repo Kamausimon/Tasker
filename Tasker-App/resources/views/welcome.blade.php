c2978c608e651fd17ecc14d62812bfa8b6d9d81c
kamau simon <kamausimon217@gmail.com>
2024-09-02 18:09:53 +0300

    Update link to dashboard in welcome.blade.php view

--- Tasker-App/resources/views/welcome.blade.php
+++ Tasker-App/resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
                 <nav class="-mx-3 flex flex-1 justify-end">
                     @auth
                     <a
-                        href="{{ url('/tasks') }}"
+                        href="{{ url('/AllTasks') }}"
                         class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
                         Dashboard
                     </a>
