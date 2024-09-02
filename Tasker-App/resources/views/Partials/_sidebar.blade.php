36360d61f9151a49b8b0e67553b433ec7cc12274
kamau simon <kamausimon217@gmail.com>
2024-09-02 04:49:50 +0300

    feat: Add recently created project to sidebar

--- Tasker-App/resources/views/Partials/_sidebar.blade.php
+++ Tasker-App/resources/views/Partials/_sidebar.blade.php
@@ -9,18 +9,17 @@
             <a href="{{ route('reports.summary') }}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700 {{ Request::routeIs('reports.summary') ? 'bg-gray-700' : '' }}">Reports</a>
             <a href="#" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700">Calendar</a>
         </nav>
+
+        <div>
+            <h6 class="text-slate-400 ml-2 text-lg font-semibold">Recently created</h6>
+            @if ($recentProject)
+            <a href="{{route('project.show')}}" class="ml-2 mt-1 text-white bg-gray-700 p-2 rounded">{{$recentProject->name}}</a>
+            @else
+            <p class="ml-2 mt-1 text-gray-400">No recently created Project</p>
+            @endif
+        </div>
     </div>
 
     <!-- Main content -->
-    <div class="flex-1 flex flex-col">
-        <!-- Toggle button -->
-        <button @click="open = !open" class="p-2 bg-gray-800 text-white rounded-md md:hidden">
-            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
-            </svg>
-        </button>
 
-        <!-- Your main content goes here -->
-
-    </div>
 </div>
