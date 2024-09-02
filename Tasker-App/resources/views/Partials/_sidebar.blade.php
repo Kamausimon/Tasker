33d2bc8876ca365375153b3620f08d7850871ff1
kamau simon <kamausimon217@gmail.com>
2024-09-02 05:19:33 +0300

    feat: Update sidebar view to display recent incomplete tasks

--- Tasker-App/resources/views/Partials/_sidebar.blade.php
+++ Tasker-App/resources/views/Partials/_sidebar.blade.php
@@ -10,12 +10,34 @@
             <a href="#" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700">Calendar</a>
         </nav>
 
-        <div>
-            <h6 class="text-slate-400 ml-2 text-lg font-semibold">Recently created</h6>
+        <div class="p-4 bg-gray-800 rounded-lg shadow-md">
+            <h6 class="text-slate-400 ml-2 text-lg font-semibold">Recently created Project</h6>
             @if ($recentProject)
-            <a href="{{route('project.show')}}" class="ml-2 mt-1 text-white bg-gray-700 p-2 rounded">{{$recentProject->name}}</a>
+            <a href="{{ route('project.show', $recentProject->id) }}" class="ml-2 mt-2 text-white bg-gray-700 p-2 rounded-md block">
+                {{$recentProject->name}}
+            </a>
             @else
-            <p class="ml-2 mt-1 text-gray-400">No recently created Project</p>
+            <p class="ml-2 mt-2 text-gray-400">No recently created Project</p>
+            @endif
+        </div>
+
+        <div class="p-4 bg-gray-800 rounded-lg shadow-md">
+            <h6 class="text-slate-400 ml-2 mt-4 text-lg font-semibold">Recent Incomplete Tasks</h6>
+            @if ($recentIncompleteTasks->isNotEmpty())
+            <ul class="ml-2 mt-2 space-y-2">
+                @foreach ($recentIncompleteTasks as $task)
+                <li class="flex items-center justify-between p-2 bg-gray-700 rounded-md">
+                    <div>
+                        <a href="{{ route('task.show', $task->id) }}" class="text-blue-400 hover:underline">
+                            {{ $task->title }}
+                        </a>
+                        <span class="text-sm text-gray-500 block">Created on {{ $task->created_at->format('Y-m-d') }}</span>
+                    </div>
+                </li>
+                @endforeach
+            </ul>
+            @else
+            <p class="ml-2 mt-2 text-gray-400">No recently created incomplete tasks.</p>
             @endif
         </div>
     </div>
