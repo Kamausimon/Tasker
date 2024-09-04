ca366dcaad0b4d5df3c521e00b1a750741eb412d
kamau simon <kamausimon217@gmail.com>
2024-09-04 17:42:05 +0300

    chore: Update _sidebar.blade.php view for improved sidebar navigation

--- Tasker-App/resources/views/Partials/_sidebar.blade.php
+++ Tasker-App/resources/views/Partials/_sidebar.blade.php
@@ -7,7 +7,7 @@
         <nav>
 
             <a href="{{ route('reports.summary') }}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700 {{ Request::routeIs('reports.summary') ? 'bg-gray-700' : '' }}">Reports</a>
-            <a href="#" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700">Calendar</a>
+            <a href="{{route('calendar')}}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700">Calendar</a>
             <a href="{{route('task.all')}}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700">Tasks</a>
             <a href="{{route('project.all')}}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700">Projects</a>
         </nav>
