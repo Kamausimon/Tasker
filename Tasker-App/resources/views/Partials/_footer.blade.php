c6f3b8669fc54df63c7dc71418e20f56c5057ac6
kamau simon <kamausimon217@gmail.com>
2024-09-05 01:20:59 +0300

    chore: Update _footer.blade.php with new background color
    This commit updates the _footer.blade.php file by changing the background color of the bottom container div. The previous color was "bg-slate-400" and it has been updated to "bg-slate-900". This modification improves the visual appearance of the footer section in the application.

--- Tasker-App/resources/views/Partials/_footer.blade.php
+++ Tasker-App/resources/views/Partials/_footer.blade.php
@@ -1,4 +1,4 @@
-<div class="fixed inset-x-0 bottom-0 bg-slate-400">
+<div class="fixed inset-x-0 bottom-0 bg-slate-900 ">
     <div class="p-3 mt-2 mr-2 mx-auto w-1/2 text-white">
         <p>Tasker &copy; <?php echo date("Y") ?></p>
     </div>
